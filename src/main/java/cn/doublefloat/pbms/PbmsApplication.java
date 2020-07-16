0bbd14ab0534b650ac92c270e07d0cddd6e4629a
Double327 <180303665@qq.com>
2020-07-16 22:25:03 +0800

    Initial commit

---
+++ src/main/java/cn/doublefloat/pbms/PbmsApplication.java
@@ -0,0 +1,17 @@
+package cn.doublefloat.pbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liguangshuai
+ */
+
+@SpringBootApplication
+public class PbmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbmsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/doublefloat/pbms/common/utils/StringUtils.java
@@ -0,0 +1,21 @@
+package cn.doublefloat.pbms.common.utils;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * 字符串工具类
+ *
+ * @author 李广帅
+ * @date 2020/7/16 9:36 下午
+ */
+
+
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+
+
+
+
+}
